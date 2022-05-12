eb9a9030a6823772b1cfd457aebbe2e0613a90a7
PRIGENT Sylvain <sylvain.prigent@inria.fr>
2022-05-12 17:52:22 +0200

    Richerdson lucy tested in 2D

--- sairyscan/enhancing/__init__.py
+++ sairyscan/enhancing/__init__.py
@@ -1,9 +1,11 @@
 from .interface import SAiryscanEnhancing
 from .gaussian import SAiryscanGaussian
 from .wiener import SAiryscanWiener
+from .richardson_lucy import SAiryscanRichardsonLucy
 from .psfs import PSFGaussian
 
 __all__ = ['SAiryscanEnhancing',
            'SAiryscanGaussian',
-           'SAiryscanWiener'
+           'SAiryscanWiener',
+           'SAiryscanRichardsonLucy'
            ]
--- sairyscan/enhancing/richardson_lucy.py
+++ sairyscan/enhancing/richardson_lucy.py
@@ -1,5 +1,6 @@
 import torch
 from .interface import SAiryscanEnhancing
+import matplotlib.pyplot as plt
 
 
 class SAiryscanRichardsonLucy(SAiryscanEnhancing):
@@ -17,25 +18,26 @@ class SAiryscanRichardsonLucy(SAiryscanEnhancing):
         self.niter = niter
 
     def __call__(self, image):
-        if image.ndims == 2:
+        if image.ndim == 2:
             fft_source = torch.fft.fft2(image)
-            psf_roll = torch.roll(self.psf, int(-self.psf.shape[0] / 2), dims=0)
-            psf_roll = torch.roll(psf_roll, int(-self.psf.shape[1] / 2), dims=1)
+            psf_roll = torch.roll(self.psf, [int(-self.psf.shape[0]/2), int(-self.psf.shape[1]/2)], dims=(0, 1))
+            # psf_roll = torch.roll(psf_roll, int(-self.psf.shape[1] / 2), dims=1)
             fft_psf = torch.fft.fft2(psf_roll)
 
-            fft_psf_mirror = torch.fft.fft2(torch.flip(self.psf, int(-self.psf.shape[0] / 2), dims=0))
+            fft_psf_mirror = torch.fft.fft2(torch.flip(psf_roll, dims=[0, 1]))
 
-            fft_out = fft_source
-            out_image = image.copy()
+            out_image = image.detach().clone()
             for i in range(self.niter):
+                fft_out = torch.fft.fft2(out_image)
                 fft_tmp = fft_out*fft_psf
-                tmp = torch.fft.ifft2(fft_tmp)
+                tmp = torch.real(torch.fft.ifft2(fft_tmp))
                 tmp = image/tmp
                 fft_tmp = torch.fft.fft2(tmp)
                 fft_tmp = fft_tmp * fft_psf_mirror
-                tmp = torch.fft.ifft2(fft_tmp)
+                tmp = torch.real(torch.fft.ifft2(fft_tmp))
+                print('tmp type=', tmp.dtype)
                 out_image = out_image * tmp
             return out_image
 
-        elif image.ndims == 3:
-            raise NotImplementedError('Richardson Lucy ED deconvolution is not yet implemented')
+        elif image.ndim == 3:
+            raise NotImplementedError('Richardson Lucy 3D deconvolution is not yet implemented')
